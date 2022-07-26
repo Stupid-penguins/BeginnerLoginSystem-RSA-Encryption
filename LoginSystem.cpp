124350d9af701f007e330218b70080cc9065b7c0
stupid <daneegameing@gmail.com>
2022-07-26 10:20:19 -0700

    The LoginSystem.cpp Is Fully Complete on Basic Functions On LINUX

--- LoginSystem.cpp
+++ LoginSystem.cpp
@@ -2,16 +2,28 @@
 #include <cstring>
 #include <fstream>
 #include <cstdlib>
-#include <direct.h>
-#include<unistd.h>
+#include <unistd.h>
 #include <sys/stat.h>
-
+#include <sstream>
 using namespace std;
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::ostringstream;
+
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
 
 void TitleOutput();
 
 int main() {
-    string User, Password, FolderName, Password_N, Password_F,R_directory ,M_Directory="User-Data/";
+    string User, Password, FolderName, FolderName_L, Password_N, R_directory, M_Directory="User-Data/";
     int AccountCreation;
     bool AccountCreated;
 
@@ -41,35 +53,52 @@ int main() {
         cout << endl << "Account Creation, Please choose a Username: ";
         cin >> User;
         FolderName = M_Directory + User;
+        FolderName_L = "mkdir " + FolderName;
 
+        system(FolderName_L.c_str());
 
-        if (mkdir(FolderName.c_str()) == -1)
-            cout << "Error creating User directory" << endl;
-        else
+        cout << endl << "What will be your password?: ";
+        cin >> Password;
+        string Password_F;
+        Password_F = FolderName + "/Password.txt";
 
-            cout << endl << "What will be your password?: ";
-            cin >> Password;
-            Password_F = FolderName + "/Password.txt";
+        ofstream FilePassword(Password_F.c_str());
+        FilePassword << Password;
+        FilePassword.close();
 
-            ofstream FilePassword(Password_F.c_str());
-            FilePassword << Password;
-            FilePassword.close();
-
-            cout << "Account Creation success" << endl;
+        cout << "Account Creation success" << endl;
 
     } else if (AccountCreation == 2) {
-        cout << "Log In" << endl;
+        cout << "Make Sure you have a account before you Log-In!" << endl;
+
     } else if (AccountCreation == 3) {
         cout << "What is your account name?: ";
+
         cin >> User;
         R_directory = "rm -r " + M_Directory + User;
         system(R_directory.c_str());
     }
+    cout << "What is your Username?: ";
+    cin >> User;
 
+    FolderName = M_Directory + User;
+    Password_N = FolderName + "/Password.txt";
 
+    string Password_F(Password_N.c_str());
+    string FileOutput;
+    FileOutput = readFileIntoString(Password_F.c_str());
 
-    system("pause");
+    cout << endl << "What is your password: ";
+    cin >> Password;
 
+    if (Password == FileOutput) {
+        cout << "Your password is correct" << endl;
+
+    }
+    else {
+        cout << "Your password is incorrect" << endl;
+
+    }
 
     return 0;
 };
